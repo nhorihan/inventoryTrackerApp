89769e0cde38e44132ed1a5436a99d8dc232c6ae
rbridge98 <ryker.bridge@gmail.com>
2022-11-21 13:43:28 -0600

    merging implementations from main and Ryker-dev

--- inventory.js
+++ inventory.js
@@ -1,20 +1,46 @@
 window.saveDataAcrossSessions = true
 
+const LOOK_DELAY = 250;
+
+let startLookTime = Number.POSITIVE_INFINITY;
+
 webgazer.setGazeListener((data, timestamp) => {
+    // restart loop if no tracking data
+    if(data == null) return
     // create an array of all the sub-elements in the inventory game-board
     var childDivs = document.getElementById('inv').getElementsByTagName('div');
-    for( i=0; i< childDivs.length; i++ ) {
+    var lookDiv
+    for( i=0; i < childDivs.length; i++ ) {
         var curDivRect = childDivs[i].getBoundingClientRect()
         // if gaze is within bounds of the current element, color should be lighter gray
         // and make a log to console
         if (data.x > curDivRect.x && data.x < curDivRect.x + curDivRect.width
             && data.y > curDivRect.y && data.y < curDivRect.y + curDivRect.height) {
-            childDivs[i].style.background="#555"
+            startLookTime = timestamp;
+            lookDiv = i
+            childDivs[i].style.background="#555";
             console.log("deez" + i);
         }
         // if not looking at element, color should be darker gray
         else {
-            childDivs[i].style.background="#444"
+            childDivs[i].style.background="#444";
+            startLookTime = Number.POSITIVE_INFINITY
         }
     }
+
+    if (startLookTime + LOOK_DELAY < timestamp) {
+        overlayOn();
+        document.getElementById("text").innerHTML = '<b>Inventory Details:</b> <br>Inventory Object ' 
+        + lookDiv + ' 1 lives here!';
+    }
+    //else overlayOff();
 }).begin()
+
+function overlayOn() {
+    console.log("on()");
+    document.getElementById("overlay").style.display = "block";
+}
+
+function overlayOff() {
+    document.getElementById("overlay").style.display = "none";
+}

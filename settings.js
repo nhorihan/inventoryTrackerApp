b3e3f578d74541a675c2b60664df079172212c7d
Justin Kaczmarek <justinkaczmarek@unomaha.edu>
2022-11-21 21:00:03 -0600

    add functionality to settings page, added settings and map and rules to inventory page(rules is not functional yet and does not have to be rules)

--- settings.js
+++ settings.js
@@ -1,6 +1,6 @@
 window.saveDataAcrossSessions = true
 
-const LOOK_DELAY = 10000;
+const LOOK_DELAY = 1000;
 
 let lookBox = null;
 let startLookTime = Number.POSITIVE_INFINITY;
@@ -12,10 +12,21 @@ webgazer.setGazeListener((data, timestamp) => {
     var game_left = document.getElementById("game").getBoundingClientRect().left + window.pageXOffset - document.getElementById("game").ownerDocument.documentElement.clientLeft;
     var game_right = document.getElementById("game").getBoundingClientRect().right + window.pageXOffset - document.getElementById("game").ownerDocument.documentElement.clientLeft;
 
+    var exit_top = document.getElementById("exit").getBoundingClientRect().top + window.pageYOffset - document.getElementById("exit").ownerDocument.documentElement.clientTop;
+    var exit_bot = document.getElementById("exit").getBoundingClientRect().bottom + window.pageYOffset - (document.getElementById("exit").ownerDocument.documentElement.clientTop);
+    var exit_left = document.getElementById("exit").getBoundingClientRect().left + window.pageXOffset - document.getElementById("exit").ownerDocument.documentElement.clientLeft;
+    var exit_right = document.getElementById("exit").getBoundingClientRect().right + window.pageXOffset - document.getElementById("exit").ownerDocument.documentElement.clientLeft;
+
     if((data.y > game_top && data.y < game_bot) && (data.x > game_left && data.x < game_right) && lookBox !== "game") {
         startLookTime = timestamp;
         lookBox = "game";
-
+    } else if ((data.y > exit_top && data.y < exit_bot) && (data.x > exit_left && data.x < exit_right) && lookBox != "exit"){
+        startLookTime = timestamp;
+        lookBox = "exit";
+    }
+    else if(data.x <= game_bot.left || data.x >= game_right){
+        startLookTime = Number.POSITIVE_INFINITY;
+        lookBox = null;
     }
 
     if(startLookTime + LOOK_DELAY < timestamp) {
@@ -23,7 +34,9 @@ webgazer.setGazeListener((data, timestamp) => {
         if(lookBox === "game") {
             // overlayOn();
             clickGame();
-        } 
+        } else if (lookBox === "exit") {
+            clickExit();
+        }
         
     }
 
@@ -36,15 +49,9 @@ var selectedSetting = 'game';
 function clickGame() {
     this.selectedSetting = 'game'
     window.location.replace("inventory.html");
-    console.log(selectedSetting);
 }
 
-function clickAudio() {
-    selectedSetting = 'audio';
-    console.log(selectedSetting);
-}
-
-function clickGraphics() {
-    selectedSetting = 'graphics';
-    console.log(selectedSetting);
+function clickExit() {
+    this.selectedSetting = 'exit';
+    window.location.replace("exit.html");
 }
